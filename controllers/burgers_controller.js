64611ad6d17cd204b140dbabfd429ac0835146fb
Jack Meier <jackson.meier423@gmail.com>
2020-07-17 21:33:01 -0700

    created router for app

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const burger = require("../models/burger.js");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  burger.selectAll((data) => {
+    const hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", (req, res) => {
+  burger.insertOne(
+    { name: req.body.burger_name, devoured: req.body.devoured },
+    (result) => {
+      res.json({ id: result.insertId });
+    }
+  );
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+  const condition = { id: req.params.id };
+
+  burger.updateOne(update, condition, (result) => {
+    if (result.affectedRows === 0) {
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  });
+});
+
+module.exports = router;
