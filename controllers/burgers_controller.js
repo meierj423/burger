033f258a11789b5b05a47f5f341f400124513638
Jack Meier <jackson.meier423@gmail.com>
2020-07-19 20:48:21 -0700

    added code that creates router for app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,5 @@
 const express = require("express");
-
 const burger = require("../models/burger.js");
-
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -9,15 +7,14 @@ router.get("/", (req, res) => {
     const hbsObject = {
       burgers: data,
     };
-    console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
 router.post("/api/burgers", (req, res) => {
   burger.insertOne(
-    ["burger_name", "devoured"],
-    [req.body.burger_name, req.body.devoured],
+    { burger_name: req.body.burger_name, devoured: req.body.devoured },
+
     (result) => {
       res.json({ id: result.insertId });
     }
@@ -25,13 +22,15 @@ router.post("/api/burgers", (req, res) => {
 });
 
 router.put("/api/burgers/:id", (req, res) => {
+  const devouredState = { devoured: req.body.devoured };
   const condition = { id: req.params.id };
 
-  burger.updateOne(update, condition, (result) => {
-    if (result.affectedRows === 0) {
+  burger.updateOne(devouredState, condition, (result) => {
+    if (result.changedRows === 0) {
       return res.status(404).end();
+    } else {
+      res.status(200).end();
     }
-    res.status(200).end();
   });
 });
 
